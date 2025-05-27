5405b45e941d72574a36523a205855c64fac5caa
FortyTwo-Dev <timeo.genna@proton.me>
2025-05-27 20:55:02 +0200

    fix config

--- config/email.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("EMAIL_HOST", "mail");
-define('EMAIL_PORT', 1025);
-define('EMAIL_SMTP_AUTH', false);
-define("EMAIL_USERNAME", "MoodSocial");
-define("EMAIL_PASSWORD", "SCXatG5J4JKEQnx3HxwJvNVM72J7F#");
