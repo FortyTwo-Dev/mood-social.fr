20604c68303334c78d3e37cc459fc7fa6574a0cd
FortyTwo-Dev <timeo.genna@proton.me>
2025-06-29 15:34:00 +0200

    easter egg

--- resources/js/post.js
+++ resources/js/post.js
@@ -12,3 +12,43 @@ postDialog.addEventListener('click', (e) =>{
         postDialog.close();
     }
 });
+
+let pressTimers = [];
+
+const shakeClasses = [
+    'shake-element-1',
+    'shake-element-2',
+    'shake-element-3',
+    'shake-element-4',
+    'shake-element-5'
+];
+
+const shakeIntervals = [3000, 5000, 7000, 9000, 11000];
+
+buttonPost.addEventListener("mousedown", () => {
+    pressTimers.forEach(timer => clearTimeout(timer));
+    pressTimers = [];
+
+    shakeIntervals.forEach((interval, index) => {
+        pressTimers.push(setTimeout(() => {
+            if (index > 0) {
+                buttonPost.classList.remove(shakeClasses[index - 1]);
+            }
+            buttonPost.classList.add(shakeClasses[index]);
+        }, interval));
+    });
+
+    pressTimers.push(setTimeout(() => {
+        buttonPost.classList.remove(shakeClasses[shakeClasses.length - 1]);
+        buttonPost.classList.add('rgb-element');
+    }, 14000));
+});
+
+buttonPost.addEventListener("mouseup", () => {
+    pressTimers.forEach(timer => clearTimeout(timer));
+    pressTimers = [];
+
+    shakeClasses.forEach(shakeClass => {
+        buttonPost.classList.remove(shakeClass);
+    });
+});
